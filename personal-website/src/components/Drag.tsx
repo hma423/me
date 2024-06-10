6015b3176fa64508efbe828a5ea599649639b22f
carb0nn <henryma@ucsb.edu>
2024-06-09 17:00:36 -0700

    randomizes the position
    can make sure that the position doesn't get into the fill in the blank part and stuff but yea

--- personal-website/src/components/Drag.tsx
+++ personal-website/src/components/Drag.tsx
@@ -5,7 +5,14 @@ import { scale, dist } from 'vec-la'
 import "./Drag.css";
 
 function Drag({ description }: { description: string }) {
-  const [{ x, y }, api] = useSpring(() => ({ x: 0, y: 0 }));
+  const randomX = (Math.random() - 0.5) * window.innerWidth* 0.5;
+  const randomY = (Math.random() - 0.5) * window.innerHeight * 0.5;
+
+  const [{ x, y }, api] = useSpring(() => ({
+    x: randomX,
+    y: randomY
+  }));
+
 
   const bind = useDrag(({ movement: [mx, my], memo = [x.get(), y.get()], velocity, direction}) => {
       api.start({ x: memo[0] + mx, y: memo[1] + my, config: {velocity: scale(direction, velocity)}});
