04510442064bb91ff81eb7cec7df2976316813c5
carb0nn <henryma@ucsb.edu>
2024-06-30 17:18:16 -0700

    homepage grid setup

--- personal-website/src/components/HomeScreen.tsx
+++ personal-website/src/components/HomeScreen.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import wii_homescreen from "../assets/audio/wii_home_startup.mp3";
-
+import "./loginscreen.css";
 
 const HomeScreen: React.FC = () => {
     const homescreen_delay = 2000; 
@@ -8,18 +8,22 @@ const HomeScreen: React.FC = () => {
         new Audio (wii_homescreen).play()
     })
   return (
-    <div className="min-h-screen bg-gray-100 text-black">
-      <div className="grid grid-cols-5 gap-4 p-4">
-        <div className="bg-gray-300 p-4 rounded"></div>
-        <div className="bg-blue-500 text-white p-4 rounded">the homebrew channel</div>
-        <div className="bg-gray-300 p-4 rounded"></div>
-        <div className="bg-gray-300 p-4 rounded"></div>
-        <div className="bg-gray-300 p-4 rounded"></div>
-        {[...Array(10)].map((_, i) => (
-          <div key={i} className="bg-gray-300 p-4 rounded"></div>
-        ))}
+    <div className="justify-center text-black">
+      <div className="wii-menu grid grid-cols-4 grid-rows-4 gap-3 pt-14">
+        <div className="bg-gray-300 p-4 rounded-2xl"></div>
+        <div className="bg-blue-500 text-white p-4 rounded-2xl">the homebrew channel</div>
+        <div className="bg-gray-300 p-4 rounded-2xl"></div>
+        <div className="bg-gray-300 p-4 rounded-2xl"></div>
+        <div className="bg-gray-300 p-4 rounded-2xl"></div>
+        <div className="bg-gray-300 p-4 rounded-2xl"></div>
+        <div className="bg-gray-300 p-4 rounded-2xl"></div>
+        <div className="bg-gray-300 p-4 rounded-2xl"></div>
+        <div className="bg-gray-300 p-4 rounded-2xl"></div>
+        <div className="bg-gray-300 p-4 rounded-2xl"></div>
+        <div className="bg-gray-300 p-4 rounded-2xl"></div>
+        <div className="bg-gray-300 p-4 rounded-2xl"></div>
       </div>
-      <div className="absolute bottom-0 left-0 right-0 bg-gray-200 p-2 flex justify-between">
+      <div className="absolute bottom-0 left-0 right-0 bg-gray-200 p-2 flex justify-between z">
         <span>Wii Menu</span>
         <span>Sat 19/09</span>
       </div>
