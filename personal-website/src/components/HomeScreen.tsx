5f8401908df1134a7d1d728ac83e785b4fa62e7e
carb0nn <henryma@ucsb.edu>
2024-06-30 23:42:12 -0700

    added delay changed backgoround colors

--- personal-website/src/components/HomeScreen.tsx
+++ personal-website/src/components/HomeScreen.tsx
@@ -1,15 +1,21 @@
 import React, { useEffect } from 'react';
 import wii_homescreen from "../assets/audio/wii_home_startup.mp3";
 import "./loginscreen.css";
+import "./homescreen.css";
 
 const HomeScreen: React.FC = () => {
-    const homescreen_delay = 2000; 
-    useEffect(()=> {
-        new Audio (wii_homescreen).play()
-    })
+  useEffect(() => {
+    const timer = setTimeout(() => {
+        new Audio(wii_homescreen).play();
+    }, 900);
+
+    // Cleanup function to clear the timer if the component unmounts
+    return () => clearTimeout(timer);
+}, []);
   return (
-    <div className="justify-center text-black">
-      <div className="wii-menu grid grid-cols-4 grid-rows-4 gap-3 pt-14">
+    <div className = "min-h-screen homescreen">
+    <div className=" justify-center text-black">
+      <div className="wii-menu grid grid-cols-4 grid-rows-4 gap-3 pt-14 -mb-40 ">
         <div className="bg-gray-300 p-4 rounded-2xl"></div>
         <div className="bg-blue-500 text-white p-4 rounded-2xl">the homebrew channel</div>
         <div className="bg-gray-300 p-4 rounded-2xl"></div>
@@ -23,10 +29,10 @@ const HomeScreen: React.FC = () => {
         <div className="bg-gray-300 p-4 rounded-2xl"></div>
         <div className="bg-gray-300 p-4 rounded-2xl"></div>
       </div>
-      <div className="absolute bottom-0 left-0 right-0 bg-gray-200 p-2 flex justify-between z">
-        <span>Wii Menu</span>
-        <span>Sat 19/09</span>
+      <div className="flex justify-center">
+        <h1>Wii Menu</h1>
       </div>
+    </div>
     </div>
   );
 };
